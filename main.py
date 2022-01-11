3f73673d8eaa92f0caaeb9cb0b9d3cb9125b208b
kevadesu <76264518+kevadesu@users.noreply.github.com>
2022-01-11 20:30:08 +0100

    :fire: Output wouldn't work, sorry.

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def setconf(fl, k, v):
                     break
         cfile.writelines(lines)
 
-editor_theme = parseconf("config/editor_theme.zc")
+editor_theme = parseconf("config/spaceduck_theme.zc")
 ext_lang_pairs = parseconf("config/lang.zc")
 debuggers = parseconf("config/debug.zc")
 
@@ -98,31 +98,12 @@ def get_file_extension(fpath):
 def get_lang(fpath):
     return get_file_extension(fpath)
 
-def run():
-    if file_path:
-        # if debugger exists for language, use that debugger, else default to python
-        # might change to just "execute the file"
-        """
-        if get_file_extension(file_path) in ext_lang_pairs and ext_lang_pairs[get_file_extension(file_path)] in debuggers:
-            command = f'{debuggers[ext_lang_pairs[get_file_extension(file_path)]]} {file_path}' 
-        else:
-            command = f'python {file_path}'
-        """
-        command = f'python {file_path}'
-        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-        output, error = process.communicate()
-        code_out.delete("1.0", END)
-        code_out.insert("1.0", output)
-        code_out.insert("1.0", error)
-    else:
-        save_as()
-
 def keybinds():
     keybinds = Toplevel(root)
     keybinds.geometry("750x250")
     keybinds.title("Keybinds")
     keybinds.configure(bg=editor_theme["popup_bg"])
-    keybindsList = Label(keybinds, bg=editor_theme["popup_bg"], fg=editor_theme["popup_fg"], text="Ctrl+S: Save\nCtrl+R: Run program\nCtrl+O: Open File\nCtrl+K: Open keybinds")
+    keybindsList = Label(keybinds, bg=editor_theme["popup_bg"], fg=editor_theme["popup_fg"], text="Ctrl+S: Save\nCtrl+O: Open File\nCtrl+K: Open keybinds")
 
     keybindsList.pack()
 
@@ -195,7 +176,6 @@ menu_bar.add_cascade(label="File", menu=file_bar)
 
 # Command Bar
 option_bar = Menu(menu_bar, tearoff=0)
-option_bar.add_command(label="Run", command=run)
 option_bar.add_command(label="Keybinds", command=keybinds)
 option_bar.add_command(label="Load Extension", command=load_extension)
 menu_bar.add_cascade(label="Option", menu=option_bar)
@@ -211,17 +191,8 @@ editor.grid(row=0, column=0, sticky="nsew")
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-# Output
-
-code_out = Text(root, height=8, width=75, bg=editor_theme["out_bg"], fg=editor_theme["out_fg"], font=editor_theme["out_font"], state="normal")
-
-code_out.grid(row=1, column=0, sticky="nsew")
-root.grid_rowconfigure(1, weight=0)
-root.grid_columnconfigure(0, weight=1)
-
 # Configure stuff
 root.bind("<Control-s>", lambda x: save_as())
-root.bind("<Control-r>", lambda x: run())
 root.bind("<Control-k>", lambda x: keybinds())
 root.bind("<Control-o>", lambda x: open_file())
 root.bind("<Control-e>", lambda x: load_extension())
